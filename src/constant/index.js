5ce40675656cba4a9c1bb1e5f0acba0bac3f267a
truongnguyen1582000 <truongnguyen1582000>
2022-04-24 20:37:48 +0700

    refactoring

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,10 +1,5 @@
 const STATIC_HOST = process.env.REACT_APP_API_URL;
-const SOCKET_HOST = process.env.REACT_APP_SOCKET_URL;
-// const STATIC_HOST = 'https://san-capstone.herokuapp.com/api';
-const secretKey = process.env.REACT_APP_SECRET_KEY;
 
 module.exports = {
   STATIC_HOST,
-  secretKey,
-  SOCKET_HOST,
 };
